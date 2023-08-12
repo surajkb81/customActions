8ddfbf42952d16b94a79b753c35fd853bdb8d09a
surajkb <suraj_kb@hotmail.com>
2023-08-12 10:55:50 +0530

    custom java script actions --> aws updated

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -5,16 +5,20 @@ function run(){{
     // deply to AWS s3
 // 1. get some input values
 // use core to get input
- core.notice("Hello from my custom Javascript Actions")
+ core.notice("Hello from my custom Javascript Actions");
  const buckets = core.getInput("bucket", {required: true});
 const bucketRegions = core.getInput("bucket-region", {required: false});
 const distFolders = core.getInput("dist-folder", {required: true});
 
 
 //2 upload files
-AWS_ACCESS_KEY_ID ="84948jkrk983039303903"
-const s3URL= 's3://${buckets}'
-execs.exec('aws s3 sync ${distFolders} ${s3URL} --region ${bucketRegions}')
+
+const s3URL= 's3://${buckets}';
+execs.exec('aws s3 sync ${distFolders} ${s3URL} --region ${bucketRegions}');
+
+// Output information
+const websiteURL = 'http://${buckets}.s3-website-${bucketRegions}.amazonaws.com';
+core.setOutput("website-url",websiteURL); //f rom setoutput
 
 }}
 run();
