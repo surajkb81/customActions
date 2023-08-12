6d154ebeff35c9bfaa5663823c14ae06b856852d
surajkb <suraj_kb@hotmail.com>
2023-08-12 09:51:02 +0530

    custom java script actions

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const execs = require('@actions/exec');
+function run(){{
+print("hello");
+core.notice("Hello from my custom Javascript Actions")
+}}
+run();
