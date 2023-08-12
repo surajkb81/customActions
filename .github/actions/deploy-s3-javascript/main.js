5fee4ac8262d0f92f385d8cd91721af717225e00
surajkb <suraj_kb@hotmail.com>
2023-08-12 10:11:03 +0530

    custom java script actions --> removed print

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const execs = require('@actions/exec');
 function run(){{
-print("hello");
+//print("hello");
 core.notice("Hello from my custom Javascript Actions")
 }}
 run();
